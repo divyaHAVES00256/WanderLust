81befcee7806e596728902a427b465893b79623a
DivyaSingh <t69967347@gmail.com>
2025-06-27 14:57:57 +0530

    Add file changes

--- index.js
+++ index.js
@@ -88,7 +88,6 @@ app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
   res.locals.currentUser = req.user; // current user for templates
-  // console.log(res.locals.success);
   next();
 });
 
